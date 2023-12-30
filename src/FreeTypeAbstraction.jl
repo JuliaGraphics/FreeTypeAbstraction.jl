d01e9044dc3de853b00d0fde997711cf0e85b1a0
TEC <git@tecosaur.net>
2023-12-30 11:14:35 +0800

    Use BaseDirs for locating font directories
    Identifying the correct platform-specific font directories can be done
    mostly correctly most of the time just with a hardcoded list of places
    to look (particularly on Apple systems). However, on Windows and
    XDG-following (i.e. Linux and friends) systems the user and system font
    directories can end up in other places, and accommodating these edge
    cases takes some effort.
    
    This effort has already been put in with the BaseDirs package, which is
    a small zero-dependency package whose entire purpose is to find the
    correct directories for different types of content on various platforms.
    So, we can remove the current font-folder-finding code entirely and just
    call BaseDirs.fonts() for a reduction in code here and an improvement in
    compatibility.

--- src/FreeTypeAbstraction.jl
+++ src/FreeTypeAbstraction.jl
@@ -5,6 +5,7 @@ using Base.Iterators: Repeated, repeated
 import Base: /, *, ==, @lock
 import Base.Broadcast: BroadcastStyle, Style, broadcasted
 using GeometryBasics: StaticVector
+using BaseDirs
 
 include("types.jl")
 include("findfonts.jl")
@@ -25,15 +26,24 @@ fontpaths() = valid_fontpaths
 function __init__()
     ft_init()
     atexit(ft_done)
+    append!(valid_fontpaths, BaseDirs.fonts(existent=true))
+    @static if Sys.isunix() && !Sys.isapple()
+        i = firstindex(valid_fontpaths)
+        while i <= length(valid_fontpaths)
+            path = valid_fontpaths[i]
+            if isdir(path)
+                append!(valid_fontpaths, filter(isdir, readdir(path, join=true)))
+            end
+            i += 1
+        end
+    end
     # This method of finding fonts might not work for exotic platforms,
     # so we supply a way to help it with an environment variable.
-    paths = filter(isdir, _font_paths())
     if haskey(ENV, "FREETYPE_ABSTRACTION_FONT_PATH")
         path = ENV["FREETYPE_ABSTRACTION_FONT_PATH"]
         isdir(path) || error("Path in environment variable FREETYPE_ABSTRACTION_FONT_PATH is not a valid directory!")
-        push!(paths, path)
+        push!(valid_fontpaths, path)
     end
-    append!(valid_fontpaths, paths)
 end
 
 if Base.VERSION >= v"1.4.2"
